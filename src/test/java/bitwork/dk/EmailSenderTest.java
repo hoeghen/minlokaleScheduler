cee76e227e282ab0979b931e37d283f9c78d572a
hoeghen <carverdk@yahoo.dk>
2015-10-11 14:15:16 +0200

    test

--- src/test/java/bitwork/dk/EmailSenderTest.java
+++ src/test/java/bitwork/dk/EmailSenderTest.java
@@ -9,14 +9,11 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by cha on 20-09-2015.
- */
 public class EmailSenderTest extends TestCase {
-    @Test
+
     public void testSendEmailApi() throws Exception {
         InputStream resourceAsStream = getClass().getResourceAsStream("/email.html");
-        String html = StreamHelper.getStringFromInputStream(resourceAsStream);
+
 
         TilbudEmail tilbudEmail = new TilbudEmail();
         List<Modtager> builder = Modtager.builder(new User("carverdk@gmail", "2791", "Dragør"), new User("carverdk@gmail", "2791", "Dragør"));
@@ -26,6 +23,6 @@ public class EmailSenderTest extends TestCase {
         tilbudEmail.tilbud.add(new Tilbud("testButikken", "hyldeengen 4, 2791 Dragør", "2791", "199", "25", "Pizza", "Lækker pizza"));
         tilbudEmail.tilbud.add(new Tilbud("testButikken2","hyldeengen 4, 2791 Dragør","2791","399","25","Pizzabrød","Super Lækker pizza"));
 
-        new HtmlEmailSender(html).sendEmailApi(tilbudEmail);
+        new HtmlEmailSender("/email.html").sendEmailApi(tilbudEmail);
     }
 }
